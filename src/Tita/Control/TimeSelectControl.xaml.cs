c735272054b58be1c5b112e471f80f17b31ec75b
희연 조 <hi2yeoni@naver.com>
2019-07-15 22:07:52 +0900

    timetable colorpart

--- src/Tita/Control/TimeSelectControl.xaml.cs
+++ src/Tita/Control/TimeSelectControl.xaml.cs
@@ -20,19 +20,53 @@ namespace Tita
     /// </summary>
     public partial class TimeSelectControl : UserControl
     {
+
         public TimeSelectControl()
         {
             InitializeComponent();
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    ColorPart colpart = new ColorPart();
+                    Grid.SetColumn(colpart, i);
+                    Grid.SetRow(colpart,j);
+
+                    colpart.MouseLeftButtonDown += Colpart_MouseLeftButtonDown;
+                    colpart.MouseRightButtonDown += Colpart_MouseRightButtonDown;
+
+                };
+            }
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Colpart_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            click(MouseButton.Left);   
 
         }
-
-        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        private void Colpart_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            click(MouseButton.Right);
         }
+
+        private void click(MouseButton btn)
+        {
+            if(btn == MouseButton.Left)
+            {
+                double x = Mouse.GetPosition(timetable).X;
+                double y = Mouse.GetPosition(timetable).Y;
+
+            }
+            else
+            {
+
+            }
+                    
+        }
+        private class ColorPart : Border
+        {
+            
+        }
+
     }
 }
